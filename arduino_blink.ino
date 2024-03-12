394d32a1a7ca56d57d03efa966630f7c4d423781
Mladen Robotics <denche-m@abv.bg>
2024-03-12 14:33:14 +0200

    Made blink_led() function. closes #4

--- arduino_blink.ino
+++ arduino_blink.ino
@@ -1,5 +1,17 @@
 #define LED_PIN 8
 
+void blink_led(int pin_number,int delay_time)
+{
+  if(pin_number < 1 || pin_number > 13) return;
+  while(true)
+  {
+    digitalWrite(pin_number,HIGH);
+    delay(delay_time);
+    digitalWrite(pin_number,LOW);
+    delay(delay_time);
+  }
+}
+
 void setup() {
 pinMode(LED_PIN, OUTPUT);
 
